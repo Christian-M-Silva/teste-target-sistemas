6510b40905896607eecdc1efa3df808307687e15
Christian Silva <tininjacms@gmail.com>
2025-01-10 10:08:11 -0300

    docs(atualizarReadme): Atualizará e formatará os readmes do projeto

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# teste-target-sistemas
+# Como executar os testes
+
+1. Após clonar o repositorio execute no terminal ``` cd atividade_numeroAtividade ```
+
+2. Execute no terminal ``` node resposta.js  ```
+    - Algumas atividades como a 3 e a 5 possuem marcações de onde alterar o valor para efetuar diferentes testes
--- atividade_2/resposta.js
+++ atividade_2/resposta.js
@@ -1,25 +1,25 @@
-function verificaFibonacci(number) {
+function verificaFibonacci(numero) {
     let fib1 = 0, fib2 = 1, proxFib = 0;
 
-    if (number === 0 || number === 1) {
-        return `O número ${number} pertence à sequência de Fibonacci.`;
+    if (numero === 0 || numero === 1) {
+        return `O número ${numero} pertence à sequência de Fibonacci.`;
     }
 
     
-    while (proxFib < number) {
+    while (proxFib < numero) {
         proxFib = fib1 + fib2; 
         fib1 = fib2;          
         fib2 = proxFib;       
     }
 
-    if (proxFib === number) {
-        return `O número ${number} pertence à sequência de Fibonacci.`;
+    if (proxFib === numero) {
+        return `O número ${numero} pertence à sequência de Fibonacci.`;
     } else {
-        return `O número ${number} não pertence à sequência de Fibonacci.`;
+        return `O número ${numero} não pertence à sequência de Fibonacci.`;
     }
 }
 
 
-const number = 40 //Altere o número
-const resultado = verificaFibonacci(number);
+const numero = 40 //Altere o número
+const resultado = verificaFibonacci(numero);
 console.log(resultado);
